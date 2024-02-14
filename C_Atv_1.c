81a9a4d190cd6ba363407e4c9696155e41b629dc
Daniel Costa Alencar <138629348+danielbsi1@users.noreply.github.com>
2024-02-14 16:47:22 -0300

    C_Atv_1.c

--- C_Atv_1.c
+++ C_Atv_1.c
@@ -5,17 +5,16 @@ int main()
 {
     printf("Bem vindo a calculadora em C");
     float numero_1;
-    printf("Digite o primeiro número: ");
+    printf("Digite o primeiro nÃºmero: ");
     scanf("%f", &numero_1);
     float numero_2;
-    printf("digite o segundo número: ");
+    printf("digite o segundo nÃºmero: ");
     scanf("%f", &numero_2);
     float divisao = numero_1 / numero_2;
     printf("%.f + %.f = %.1f\n", numero_1, numero_2, numero_1 + numero_2);
     printf("%.f / %.f = %.1f\n", numero_1, numero_2, divisao);
     printf("%.f X %.f = %.1f\n", numero_1, numero_2, numero_1 * numero_2);
     printf("%.f - %.f = %.1f\n", numero_1, numero_2, numero_1 - numero_2);
-    return 0;
 
     return 0;
 }
